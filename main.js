3c69d1ae95af49459ad3d4e262825aaad6e1deb1
dharmikbhanushali <45991249+dharmikbhanushali@users.noreply.github.com>
2019-12-27 12:33:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function loginFunc(){
+    document.getElementById("login-form").style.visibility = "visible";
+    document.getElementById("signup-form").style.visibility = "hidden";
+}
+
+function signupFunc(){
+    document.getElementById("signup-form").style.visibility = "visible";
+    document.getElementById("login-form").style.visibility = "hidden";
+}
